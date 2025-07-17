fe6543ff71449d44412afed133e7c396cdccb7cf
orlyprofili <orly@profilidesign.com>
2025-07-17 17:50:12 -0400

    adafruit variant. comes up but connection fails with pageset issues

--- examples/iperf/main/src/lwipopts_myfix.h
+++
@@ -1,9 +0,0 @@
-// UNUSED
-/*  components/lwip/include/lwipopts_myfix.h
- *
- *  Force software ICMP checksums on any netif that *doesn't* say otherwise.
- *  Works around MM6108 off-load bug (echo replies had checksum = 0).
- */
-#define LWIP_CHECKSUM_CTRL_PER_NETIF   1
-#define CHECKSUM_CHECK_ICMP            0     /* accept zero-checksum frames   */
-#define CHECKSUM_GEN_ICMP              1     /* generate checksum in software */
